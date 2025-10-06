7cc59f10e49266288499dc87864332e549fedc78
anlyons <anlyons@ucsd.edu>
2025-10-06 00:39:38 -0700

    Quick load bidmc data function

---
+++ src/quick_load_bidmc.m
@@ -0,0 +1,30 @@
+function [ppg,resp,ekg,fs,meta] = quick_load_bidmc(dataFile)
+% Input (optional): path to bidmc_data.mat
+% Output cell arrays of ppg, resp, ekg, and meta for all 53 patients
+
+    if nargin < 1
+        dataFile = fullfile(fileparts(pwd),"data","bidmc_data.mat");
+    end
+
+    s = load(dataFile);
+    data = s.data; 
+    nRecords = numel(data);
+
+    % preallocate cell arrays
+    ppg  = cell(nRecords,1);
+    ekg  = cell(nRecords,1);
+    resp = cell(nRecords,1);
+    fs   = zeros(nRecords,1);
+    meta = cell(nRecords,1);
+    
+    for i = 1:nRecords
+        rec = data(i);
+        
+        ppg{i}  = rec.ppg.v(:);
+        ekg{i}  = rec.ekg.v(:);
+        resp{i} = rec.ref.resp_sig.imp(:);
+        fs(i)   = rec.ppg.fs;   % usually 125 Hz
+        meta{i}     = rec.fix;      % subject ID, location, etc.
+    end
+end
+
